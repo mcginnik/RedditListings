a09babb7e22477e37f94861403f3e17547ab2985
Kyle McGinnis <kyle.mcginnis@gmail.com>
2023-02-22 07:49:05 -0800

    removed storyboard and added home navigation setup

--- RedditListings/RedditListings/SceneDelegate.swift
+++ RedditListings/RedditListings/SceneDelegate.swift
@@ -15,8 +15,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        /// 4. Set the root view controller of the window with your view controller
+        window.rootViewController = UINavigationController(rootViewController: HomeViewController())
+        /// 5. Set the window and call makeKeyAndVisible()
+        self.window = window
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
