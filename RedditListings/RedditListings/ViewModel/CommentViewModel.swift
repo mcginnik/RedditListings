a7568ea838d49e35d2e0db27ce88e92fde6b3298
Kyle McGinnis <kyle.mcginnis@gmail.com>
2023-02-25 14:21:19 -0800

    ItemViewModel to simplify comment and post viewmodels

--- RedditListings/RedditListings/ViewModel/CommentViewModel.swift
+++ RedditListings/RedditListings/ViewModel/CommentViewModel.swift
@@ -8,68 +8,17 @@
 import UIKit
 import Combine
 
-class CommentViewModel: ObservableObject {
+class CommentViewModel: ItemViewModel<Comment> {
     
     // MARK: Properties
     
-    let model: Comment
-    
-    var id: String {
-        return model.id
-    }
-    
-    var body: String? {
-        return model.body
-    }
-    
-    var title: String {
-        model.title ?? ""
-    }
-    
-    var caption: String {
-        if let author = model.author {
-            return "@\(author)"
-        }
-        return ""
-    }
-    
-    var subtitle: String? {
-        return model.subtitle
-    }
-    
-    var imageURL: String? {
+    override var imageURL: String? {
         let isValidURL = model.url?.isValid(regex: .jpgImageURL) ?? false
         return isValidURL ? model.url : model.thumbnail
-    }
-    
-    var createdAt: Double {
-        model.created ?? 0
-    }
-            
-    // MARK: Lifecycle
-    
-    required init(withData model: Comment){
-        self.model = model
     }
     
 }
 
 extension CommentViewModel: ListCellDataProtocol {
     typealias P = CommentPage
-}
-
-extension CommentViewModel: Hashable, Comparable {
-
-    static func == (lhs: CommentViewModel, rhs: CommentViewModel) -> Bool {
-        lhs.id == rhs.id
-    }
-    
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(id)
-    }
-    
-    static func < (lhs: CommentViewModel, rhs: CommentViewModel) -> Bool {
-        lhs.createdAt < rhs.createdAt
-    }
-    
 }
--- RedditListings/RedditListings/ViewModel/PostViewModel.swift
+++ RedditListings/RedditListings/ViewModel/PostViewModel.swift
@@ -8,68 +8,8 @@
 import UIKit
 import Combine
 
-class PostViewModel: ObservableObject {
-    
-    // MARK: Properties
-    
-    let model: Post
-    
-    var id: String {
-        return model.id
-    }
-    
-    var body: String? {
-        return model.body
-    }
-    
-    var title: String {
-        return model.title ?? ""
-    }
-    
-    var subtitle: String? {
-        model.subtitle
-    }
-    
-    var caption: String {
-        if let author = model.author {
-            return "@\(author)"
-        }
-        return ""
-    }
-    
-    var imageURL: String? {
-        model.thumbnail
-    }
-    
-    var createdAt: Double {
-        model.created ?? 0
-    }
-            
-    // MARK: Lifecycle
-    
-    required init(withData model: Post){
-        self.model = model
-    }
-
-    
-}
+class PostViewModel: ItemViewModel<Post> {}
 
 extension PostViewModel: ListCellDataProtocol {
     typealias P = PostPage
-}
-
-extension PostViewModel: Hashable, Comparable {
-
-    static func == (lhs: PostViewModel, rhs: PostViewModel) -> Bool {
-        lhs.id == rhs.id
-    }
-    
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(id)
-    }
-    
-    static func < (lhs: PostViewModel, rhs: PostViewModel) -> Bool {
-        lhs.createdAt < rhs.createdAt
-    }
-    
 }
